e6638ecb40c85dcc08f38e2b81316a66eff2834f
Almar <almar456@hotmail.com>
2018-09-12 16:22:27 +0200

    Opdrachten t/m 28 September toegevoegd. (FA klaar)

--- 28 Sept/FA_NS-functies.py
+++ 28 Sept/FA_NS-functies.py
@@ -3,6 +3,41 @@ def standaardprijs(afstandkm: float):
         prijs = float(afstandkm*0.8)
     elif (afstandkm <= 0):
         prijs = 0
-    else
+    else:
         prijs = float(15.0+(0.6*afstandkm))
     return prijs
+
+def ritprijs(leeftijd: int, weekendrit: bool, afstandkm: float):
+    prijs = standaardprijs(afstandkm)
+    if (leeftijd >= 12) and (leeftijd < 65) and (weekendrit == True):
+        prijs = prijs * 0.6
+    elif ((leeftijd < 12) or (leeftijd >= 65)) and (weekendrit == True):
+        prijs = prijs * 0.65
+    elif ((leeftijd < 12) or (leeftijd >= 65)) and (weekendrit == False):
+        prijs = prijs * 0.7
+    return prijs
+
+print(ritprijs(11,False,-20))
+print(ritprijs(11,False,20))
+print(ritprijs(11,False,70))
+print(ritprijs(11,True,-20))
+print(ritprijs(11,True,20))
+print(ritprijs(11,True,70))
+print(ritprijs(12,False,-20))
+print(ritprijs(12,False,20))
+print(ritprijs(12,False,70))
+print(ritprijs(12,True,-20))
+print(ritprijs(12,True,20))
+print(ritprijs(12,True,70))
+print(ritprijs(64,False,-20))
+print(ritprijs(64,False,20))
+print(ritprijs(64,False,70))
+print(ritprijs(64,True,-20))
+print(ritprijs(64,True,20))
+print(ritprijs(64,True,70))
+print(ritprijs(65,False,-20))
+print(ritprijs(65,False,20))
+print(ritprijs(65,False,70))
+print(ritprijs(65,True,-20))
+print(ritprijs(65,True,20))
+print(ritprijs(65,True,70))
