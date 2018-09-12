95033addf4fc2e02ffc829cb091b14d8f6f6fc0e
Almar <almar456@hotmail.com>
2018-09-12 12:32:31 +0200

    Opdrachten t/m 28 September toegevoegd. (2.6 af en 2.3 bijgewerkt.)

--- 28 Sept/2.6 Functie met (im)mutable parameter.py
+++ 28 Sept/2.6 Functie met (im)mutable parameter.py
@@ -1,5 +1,7 @@
 def wijzig(letterlijst: list):
-    return globals()['d','e','f']
+    letterlijst[0] = 'd'
+    letterlijst[1] = 'e'
+    letterlijst[2] = 'f'
 
 lijst = ['a','b','c']
 print(lijst)
