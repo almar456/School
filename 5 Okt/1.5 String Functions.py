358b3c06b854c092d646262e2d2716e8e793dd52
Almar <almar456@hotmail.com>
2018-10-04 12:39:34 +0200

    Opdrachten 1.5, 5 okt verbeterd

--- 5 Okt/1.5 String Functions.py
+++ 5 Okt/1.5 String Functions.py
@@ -1,46 +1,10 @@
-def removeLeestekens(woordenlijst: list):
-    index = -1
-    mutable = []
-    for x in woordenlijst:
-        index = index + 1
-        if ('.' in x) or (',' in x) or ('!' in x) or ('?' in x):
-            mutable = list(x)
-            newword = str('')
-            lcount = -1
-            if '.' in mutable:
-                mutable[mutable.index('.')] = ''
-                for x in mutable:
-                    lcount = lcount + 1
-                    newword = newword + x
-                woordenlijst[index] = newword
-            elif ',' in mutable:
-                mutable[mutable.index(',')] = ''
-                for x in mutable:
-                    lcount = lcount + 1
-                    newword = newword + x
-                woordenlijst[index] = newword
-            elif '!' in mutable:
-                mutable[mutable.index('!')] = ''
-                for x in mutable:
-                    lcount = lcount + 1
-                    newword = newword + x
-                woordenlijst[index] = newword
-            elif '?' in mutable:
-                mutable[mutable.index('?')] = ''
-                for x in mutable:
-                    lcount = lcount + 1
-                    newword = newword + x
-                woordenlijst[index] = newword
-
-def gemiddelde():
-    string = input('Voer hier een willekeurige zin in:\n')
-    woordenlijst = string.split(' ')
-    removeLeestekens(woordenlijst)
-    wcount = len(woordenlijst)
-    gemLengte = 0
-    for x in woordenlijst:
-        gemLengte = gemLengte + len(x)
-    gemLengte = (gemLengte/wcount)
-    print(gemLengte)
-
-gemiddelde()
+string = input('Voer hier een willekeurige zin in:\n')
+string = string.replace(',','')
+string = string.replace('.','')
+woordenlijst = string.split(' ')
+wcount = len(woordenlijst)
+gemLengte = 0
+for x in woordenlijst:
+    gemLengte = gemLengte + len(x)
+gemLengte = (gemLengte/wcount)
+print(gemLengte)
